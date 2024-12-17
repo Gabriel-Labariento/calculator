6ce0789fb0786546adec1e9bc7d2bb0f48074188
Gabriel Labariento <gabrielmatthew.labariento@gmail.com>
2024-12-17 20:29:41 +0800

    Temporary functional model for calculator.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let numbers = Array.from(document.querySelectorAll(".number"));
 let operations = Array.from(document.querySelectorAll(".operation"));
-
+let buttons = Array.from(document.querySelectorAll(".col"));
 let screen = document.querySelector(".screen")
 
 let inputtedNum1 = false;
 let inputtedNum2 = false
 let inputtedOperation = false;
 let hasDecimalClicked = false;
+let num1;
+let num2;
+let operation;
 
 function clear(){
     inputtedNum1 = false;
@@ -17,19 +20,26 @@ function clear(){
 }
 
 function displayNumber(e){
+    // Check for decimal point
     if (e.target.textContent === "."){
         if (screen.textContent.includes(".")) return
     }
+    // Check if num1 or num2
+    if (inputtedOperation) screen.textContent = "";
     screen.textContent += e.target.textContent;
 }
 
 function rememberOperation(e){
     if (!inputtedOperation){
+        num1 = parseFloat(screen.textContent);
         let operation = e.target;
         operation.style.backgroundColor = "#0A0908"
         inputtedOperation = true;
-    } 
-    
+        inputtedOperation = e.target.textContent;
+    } else {
+        num2 = parseFloat(screen.textContent);
+        console.log(num2);
+    }
 }
 
 function operate(num1, num2, operation){
@@ -63,7 +73,3 @@ operations.map((operation) =>{
     operation.addEventListener ("click", 
         (operation.textContent === "AC") ? clear : rememberOperation);
 })
-
-
-
-
